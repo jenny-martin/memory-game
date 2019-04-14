6e1b32bb0046ca2493458970cfa39259eee600c6
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-04-14 15:05:23 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "Queen of Hearts";
+var cardTwo = "Queen of Diamonds";
+var cardThree = "King of Hearts";
+var cardFour = "King of Diamonds";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
