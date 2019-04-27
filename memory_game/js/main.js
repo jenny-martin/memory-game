5b984e26b1ec90348fdcdad68dab981d68e5810f
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-04-27 18:38:07 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [0];
+cardsInPlay.push(cards[0]);
 
-console.log("Up and running!");
+console.log("User flipped " + cardsInPlay);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardTwo = [2];
 
-console.log("user flipped" + " " + cardOne);
-console.log("user flipped" + " " + cardThree);
+cardsInPlay.push(cards[2]);
+
+
+console.log("User flipped " + cardsInPlay);
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
+alert('You found a match!');
+} else (cardsInPlay[0] === cardsInPlay[1] || cardsInPlay[0] === cardsInPlay[3])
+  alert('Sorry, try again.');
