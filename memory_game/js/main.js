e7299de0a633b2316f90217a9a010e7a1fc74997
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-05-07 21:58:18 -0500

    finally got this working correctly

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -17,31 +17,44 @@ const cards = [
 {
     rank: 'king',
     suit: 'diamonds',
-    cardImage: "images/queen-of-diamonds.png"
+    cardImage: "images/king-of-diamonds.png"
 }
 ];
 
-
 const cardsInPlay = [];
 
-const checkForMatch = function() {
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+
+
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
-} else {
-alert("Sorry, try again.");
+    } else {
+    alert("Sorry, try again.");
+    }
+    };
+
+const flipCard = function() {
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+if (cardsInPlay.length === 2) {
+    checkForMatch();
+    }
+};
+
+const createBoard = function() {
+    var gameBoard = document.getElementById("game-board");
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener("click", flipCard);
+        gameBoard.appendChild(cardElement);
+    }
 }
-}
+    
 
-const flipCard = function(cards) {
-console.log("User flipped " + cards.rank);
-console.log(cards.cardImage);
-console.log(cards.suit);
-}
-
-
-cardsInPlay.push(cards.rank);
-
-flipCard(cards[0]);
-flipCard(cards[2]);
-
-checkForMatch();
+createBoard();
